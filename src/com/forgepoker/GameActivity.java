4c1ca0f6bc532efc34e4fb04aa0f62b4d01eae95
Maowen <maowen.zhang@hotmail.com>
2013-11-07 23:53:43 +0800

    create StartActivity & GameActivity

--- src/com/forgepoker/GameActivity.java
+++ src/com/forgepoker/GameActivity.java
@@ -2,26 +2,18 @@ package com.forgepoker;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
-import com.forgepoker.R;
 
 /**
- * Main activity for poker game, includes elements like game table, startup menu, etc.
+ * Game activity for poker game, includes elements like game table, startup menu, etc.
  * @author zhanglo
  *
  */
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
-	public final static int MENU=0;
-	public final static int GAME=1;
-	public final static int RESULT=2;
-	
-	private GameView gameView;
-	private StartupView startupView;
+	GameView gameView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +23,16 @@ public class MainActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		//gameView = new GameView(this, this);
-		startupView = new StartupView(this);
-		
-		setContentView(startupView);		
-//		setContentView(R.layout.activity_main);		
+//		gameView = new GameView(this);
+//		setContentView(gameView);
+		setContentView(R.layout.activity_game);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
 	}
+
 }
---
+++ src/com/forgepoker/StartActivity.java
@@ -0,0 +1,29 @@
+package com.forgepoker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class StartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start, menu);
+		return true;
+	}
+	
+	public void onClickStartGame(View v) {
+		Intent intent = new Intent(this, GameActivity.class);
+		startActivity(intent);
+	}
+	
+}
