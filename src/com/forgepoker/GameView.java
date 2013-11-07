fe3914448ec61002b73af1f06a9e5a6893ba7f08
Maowen <maowen.zhang@hotmail.com>
2013-11-07 21:46:58 +0800

    rename package and post some resources

---
+++ src/com/forgepoker/GameView.java
@@ -0,0 +1,103 @@
+package com.forgepoker;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback,
+		OnTouchListener {
+	
+	MainActivity ddz;
+	boolean threadFlag=true;
+	
+//	Desk desk;
+	
+	SurfaceHolder holder;
+	Canvas canvas;
+
+	Bitmap gameBack;
+	Thread gameThread = new Thread() {
+		@Override
+		public void run() {
+			holder=getHolder();
+			while(threadFlag)
+			{
+//				desk.gameLogic();
+				
+				try {
+					canvas = holder.lockCanvas();
+//					onDraw(canvas);
+					draw(canvas);
+				} finally {
+					holder.unlockCanvasAndPost(canvas);
+				}
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	};
+
+	public GameView(Context context, MainActivity ddz) {
+		super(context);
+		this.ddz = ddz;
+//		desk=new Desk(ddz);
+//		gameBack=BitmapFactory.decodeResource(getResources(), R.drawable.vbg2);
+		this.getHolder().addCallback(this);
+		this.setOnTouchListener(this);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+//		System.out.println("in method onDraw");
+		canvas.drawBitmap(gameBack, 0, 0, null);
+//		desk.paint(canvas);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		threadFlag=true;
+		gameThread.start();
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		threadFlag=false;
+		boolean retry = true;
+		while (retry) {
+			try {
+				gameThread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if(event.getAction()!=MotionEvent.ACTION_UP)
+		{
+			return true;
+		}
+		System.out.println(event.getX() + "  " + event.getY()+"-"+(event.getAction()==MotionEvent.ACTION_UP));
+//		desk.onTuch(v, event);
+//		threadFlag=!threadFlag;
+		return true;
+	}
+}
---
+++ src/com/forgepoker/MainActivity.java
@@ -0,0 +1,65 @@
+package com.forgepoker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import com.forgepoker.R;
+
+public class MainActivity extends Activity {
+
+	public final static int MENU=0;
+	public final static int GAME=1;
+	public final static int RESULT=2;
+	
+	private GameView mGameView;
+	private StartupView mStartView;
+	private MainActivity mMain;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		mMain = this;
+		mGameView = new GameView(this, this);
+		mStartView = new StartupView(this, this);
+		
+		setContentView(mStartView);		
+//		setContentView(R.layout.activity_main);		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	Handler handler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+
+			switch (msg.what) {
+				case 0:
+					setContentView(mStartView);
+					break;
+				case 1:
+					setContentView(mGameView);
+					break;
+				case 2:
+					break;
+			}
+		}
+
+	};
+
+}
